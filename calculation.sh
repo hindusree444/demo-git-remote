0bec6d73b81bd4a57cbeff58884e6a5b083f8d5d
hindu sree <hdunaboyina@softility.com>
2025-01-29 10:27:22 +0530

    adding substration expression

---
+++ calculation.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Enter first number:"
+read num1
+
+echo "Enter an operator (+, -, *, /):"
+read operator
+
+echo "Enter second number:"
+read num2
+
+# Perform the calculation
+result=$(echo "$num1 $operator $num2" | bc)
+
+echo "Result: $result"
+
+
